2cb2bf6c76993cad4e43054f1a75e981ef7c0f04
dingjingMaster <dingjing@live.cn>
2018-01-10 17:15:16 +0800

    修正sql错误

--- sql/create_retention.sql
+++ sql/create_retention.sql
@@ -1,31 +1,33 @@
-
+alter database item_retention default character set utf8;
 CREATE TABLE IF NOT EXISTS `item_retent_status`(
     `irid` VARCHAR(128) NOT NULL COMMENT '主键 状态+分类(付费情况)+时间戳',
     `last` INT(12) DEFAULT 0 NOT NULL COMMENT '上一阶段人数',
     `remain` INT(12) DEFAULT 0 NOT NULL COMMENT '留下人数',
-    `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别: 1免费 2付费 3包月 4公版 5限免',
-    `typeCate` INT(6) DEFALUT 0 NOT NULL COMMENT '统计类型: 1天人数 2周人数 3七日人数',
+    `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别 1免费 2付费 3包月 4公版 5限免',
+    `typeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '统计类型 1天人数 2周人数 3七日人数',
     `timeStamp` INT(10) NOT NULL COMMENT '时间戳20180110',
     PRIMARY KEY (`irid`)
     );
+alter table item_retent_status default character set utf8;
 
 CREATE TABLE IF NOT EXISTS `item_retent_viewCount`(
     `irid` VARCHAR(128) NOT NULL COMMENT '主键 订阅+分类(付费情况)+时间戳',
     `last` INT(12) DEFAULT 0 NOT NULL COMMENT '上一阶段人数',
     `remain` INT(12) DEFAULT 0 NOT NULL COMMENT '留下人数',
     `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别: 1免费 2付费 3包月 4公版 5限免',
-    `typeCate` INT(6) DEFALUT 0 NOT NULL COMMENT '统计类型: 1天人数 2周人数 3七日人数',
+    `typeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '统计类型: 1天人数 2周人数 3七日人数',
     `viewCate` INT(6) DEFAULT 0 NOT NULL COMMENT '订阅量级别: 1小于100 2是1b-1k 3是1k-1w 4是1w-10w 5是大于10w',
     `timeStamp` INT(10) NOT NULL COMMENT '时间戳20180110',
     PRIMARY KEY (`irid`)
     );
+alter table item_retent_viewCount default character set utf8;
 
 CREATE TABLE IF NOT EXISTS `item_retent_intime`(
     `irid` VARCHAR(128) NOT NULL COMMENT '主键 入库时间+分类(付费情况)+时间戳',
     `last` INT(12) DEFAULT 0 NOT NULL COMMENT '上一阶段人数',
     `remain` INT(12) DEFAULT 0 NOT NULL COMMENT '留下人数',
     `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别: 1免费 2付费 3包月 4公版 5限免',
-    `typeCate` INT(6) DEFALUT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
+    `typeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
     `intimeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '入库时间级别: 1小于1月 2是1月-3月 3是3月-1年 4是大于1年',
     `timeStamp` INT(10) NOT NULL COMMENT '时间戳20180110',
     PRIMARY KEY (`irid`)
@@ -36,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `item_retent_update`(
     `last` INT(12) DEFAULT 0 NOT NULL COMMENT '上一阶段人数',
     `remain` INT(12) DEFAULT 0 NOT NULL COMMENT '留下人数',
     `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别: 1免费 2付费 3包月 4公版 5限免',
-    `typeCate` INT(6) DEFALUT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
+    `typeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
     `updateCate` INT(6) DEFAULT 0 NOT NULL COMMENT '入库时间级别: 1小于1月 2是1月-3月 3是3月-1年 4是大于1年',
     `timeStamp` INT(10) NOT NULL COMMENT '时间戳20180110',
     PRIMARY KEY (`irid`)
@@ -47,10 +49,13 @@ CREATE TABLE IF NOT EXISTS `item_retent_classify1`(
     `last` INT(12) DEFAULT 0 NOT NULL COMMENT '上一阶段人数',
     `remain` INT(12) DEFAULT 0 NOT NULL COMMENT '留下人数',
     `feeCate` INT(3) DEFAULT 0 NOT NULL COMMENT '付费类别: 1免费 2付费 3包月 4公版 5限免',
-    `typeCate` INT(6) DEFALUT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
+    `typeCate` INT(6) DEFAULT 0 NOT NULL COMMENT '统计类型: 1天人数 2天留下人数 3周人数 4周留下人数 5七日人数 6七日留下人数',
     `updateCate` INT(6) DEFAULT 0 NOT NULL COMMENT '一级分类: 1男频 2女频 3出版 4其它',
     `timeStamp` INT(10) NOT NULL COMMENT '时间戳20180110',
     PRIMARY KEY (`irid`)
     );
 
+alter table item_retent_intime default character set utf8;
+alter table item_retent_update default character set utf8;
+alter table item_retent_classify1 default character set utf8;
 
