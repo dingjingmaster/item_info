e711223367aceba19295f4981d5d6d32f7315842
dingjingMaster <dingjing@live.cn>
2018-01-22 14:24:28 +0800

    x轴时间戳问题

--- include/item_exhibit_info.php
+++ include/item_exhibit_info.php
@@ -350,7 +350,7 @@ function get_chart_json($which, $div) {
     
             // 生成x
             $ret = generate_x($xArray);
-            if($ret != false && (count($ret) <= 0)) {
+            if($ret != false && (count($ret) > 1)) {
                 $xData = $ret;
             }
     
@@ -377,7 +377,7 @@ function get_chart_json($which, $div) {
                 }
                 // 生成x
                 $ret = generate_x($xArray);
-                if($ret != false && (count($ret) <= 1)){
+                if($ret != false && (count($ret) > 1)){
                     $xData = $ret;
                 }
                 // 生成y
@@ -404,7 +404,7 @@ function get_chart_json($which, $div) {
                     }
                     // 生成 x
                     $ret = generate_x($xArray);
-                    if($ret != false && (count($ret) <= 0)){
+                    if($ret != false && (count($ret) > 1)){
                         $xData = $ret;
                     }
                     // 生成 y
