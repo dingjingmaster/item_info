82e297c329d57893f090048f01fe6f8c353f8f85
dingjingMaster <dingjing@live.cn>
2018-01-23 14:29:54 +0800

    小修改

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -14,17 +14,20 @@ if(!strcasecmp(INTEST, 'DEBUG')) {
 
 function search_init(){
     echo '<form class="layui-form" action="">' .
+
             '<div class="layui-form-item">' . 
                 '<label class="layui-form-label">选择查询维度</label>' .
                 '<div class="layui-input-block">' . 
-                    '<input type="radio" name="dim" value="sumDim" title="模块总计">' . 
-                    '<input type="radio" name="dim" value="feeDim" title="付费类型">' .
-                    '<input type="radio" name="dim" value="stgDim" title="推荐策略">' .
-                    '<input type="radio" name="dim" value="stuDim" title="连载状态">' .
-                    '<input type="radio" name="dim" value="vewDim" title="订阅级别">' .
-                    '<input type="radio" name="dim" value="itmDim" title="入库时间">' .
-                    '<input type="radio" name="dim" value="utmDim" title="更新时间">' .
-                    '<input type="radio" name="dim" value="ctg1Dim" title="一级分类">'.
+                    '<select name="dim" lay-filter="">' .
+                        '<option value="sumDim">总计</option>' . 
+                        '<option value="feeDim">付费类型</option>' .
+                        '<option value="stgDim">推荐策略</option>' .
+                        '<option value="stuDim">连载状态</option>' .
+                        '<option value="vewDim">订阅级别</option>' .
+                        '<option value="itmDim">入库时间</option>' .
+                        '<option value="utmDim">更新时间</option>' .
+                        '<option value="ctg1Dim">一级分类</option>'.
+                    '</select>' .
                 '</div>' .
             '</div>' .
 
