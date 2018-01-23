f71f50afd8ba76ffc29a632bdd19ea4b9fc27b98
dingjingMaster <dingjing@live.cn>
2018-01-23 15:01:00 +0800

    小修改

--- index.php
+++ index.php
@@ -70,7 +70,10 @@ require dirname(__FILE__) . '/include/common_action.php';
 
             <div class="layui-body">
                 <div id="form_div" style="padding: 15px;"></div>
-                <div id="main_div" style="padding: 15px;"></div>
+                <div id="main_div" style="padding: 15px;">
+
+<form class="layui-form" action=""><div class="layui-form-item"><label class="layui-form-label">选择查询维度</label><div class="layui-input-block"><select name="dim" lay-filter=""><option value="sumDim">总计</option><option value="feeDim">付费类型</option><option value="stgDim">推荐策略</option><option value="stuDim">连载状态</option><option value="vewDim">订阅级别</option><option value="itmDim">入库时间</option><option value="utmDim">更新时间</option><option value="ctg1Dim">一级分类</option></select></div></div><div class="layui-form-item"><button class="layui-btn" layui-submit="">提交</button></div></form><hr class="layui-bg-black"/>
+</div>
             </div>
 
             <div class="layui-footer"> @  宜搜小说 </div>
