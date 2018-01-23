6b9d42f4ad04032e58c4647dfbe375e034500563
dingjingMaster <dingjing@live.cn>
2018-01-23 17:20:01 +0800

    小修改

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -15,33 +15,55 @@ if(!strcasecmp(INTEST, 'DEBUG')) {
 function search_init(){
     $page = '<form class="layui-form" action="">'.
                 '<div class="layui-form-item">'.
-                    '<label class="layui-form-label">要查询的模块</label>'.
+                    '<label class="layui-form-label">查询维度</label>'.
                     '<div class="layui-input-block">'.
-                        '<select name="city" lay-verify="required">'.
+                        '<select name="dim" lay-verify="required">'.
                             '<option value=""></option>'.
-                            '<option value="0">北京</option>'.
-                            '<option value="1">上海</option>'.
-                            '<option value="2">广州</option>'.
-                            '<option value="3">深圳</option>'.
-                            '<option value="4">杭州</option>'.
+                            '<option value="1">总计</option>'.
+                            '<option value="2">付费类型</option>'.
+                            '<option value="3">推荐策略</option>'.
+                            '<option value="4">连载状态</option>'.
+                            '<option value="5">订阅级别</option>'.
+                            '<option value="6">入库时间</option>'.
+                            '<option value="7">更新时间</option>'.
+                            '<option value="8">一级分类</option>'.
                         '</select>'.
                     '</div>'.
                     '</div>'.
                 '<div class="layui-form-item">'.
-                    '<label class="layui-form-label">复选框</label>'.
+                    '<label class="layui-form-label">页面模块</label>'.
                     '<div class="layui-input-block">'.
-                        '<input type="checkbox" name="like[write]" title="写作">'.
-                        '<input type="checkbox" name="like[read]" title="阅读" checked>'.
-                        '<input type="checkbox" name="like[dai]" title="发呆">'.
+                        '<input type="checkbox" name="shfRecMdl" title="书架推荐">'.
+                        '<input type="checkbox" name="freGusMdl" title="免费-猜你喜欢">'.
+                        '<input type="checkbox" name="bakArdMdl" title="章末页-读本书的人还看过">'.
+                        '<input type="checkbox" name="foeArdMdl" title="封面页-读本书的人还看过">'.
+                        '<input type="checkbox" name="shfGusMdl" title="书架-猜你喜欢">'.
+                        '<input type="checkbox" name="chsStmMdl" title="精选-瀑布流">'.
+                        '<input type="checkbox" name="foeArdMorMdl" title="封面页-读本书的人还看过更多">'.
+                        '<input type="checkbox" name="monStmMdl" title="包月瀑布流">'.
+                        '<input type="checkbox" name="foeCtgMdl" title="封面页-类别推荐">'.
+                        '<input type="checkbox" name="foeAutMdl" title="封面页-作者推荐">'.
+                        '<input type="checkbox" name="chsFinStmMdl" title="精选-完结瀑布流">'.
+                        '<input type="checkbox" name="freMonRecMdl" title="免费-包月推荐">'.
+                        '<input type="checkbox" name="chsBoyStmMdl" title="精选-男频瀑布流">'.
+                        '<input type="checkbox" name="chsRakStmMdl" title="精选-排行瀑布流">'.
+                        '<input type="checkbox" name="chsGilStmMdl" title="精选-女频瀑布流">'.
+                        '<input type="checkbox" name="redRecMdl" title="根据阅读推荐">'.
+                        '<input type="checkbox" name="extRecMdl" title="退出拦截推荐">'.
                     '</div>'.
                 '</div>'.
+
                 '<div class="layui-form-item">'.
-                    '<label class="layui-form-label">单选框</label>'.
+                    '<label class="layui-form-label">付费类型</label>'.
                     '<div class="layui-input-block">'.
-                        '<input type="radio" name="sex" value="男" title="男">'.
-                        '<input type="radio" name="sex" value="女" title="女" checked>'.
+                        '<input type="checkbox" name="freFee" title="免费">'.
+                        '<input type="checkbox" name="chgFee" title="付费">'.
+                        '<input type="checkbox" name="monFee" title="包月">'.
+                        '<input type="checkbox" name="pubFee" title="公版">'.
+                        '<input type="checkbox" name="tfFee" title="限免">'.
                     '</div>'.
                 '</div>'.
+
                 '<div class="layui-form-item">'.
                     '<div class="layui-input-block">'.
                         '<button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>'.
