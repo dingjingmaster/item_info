85ff6691c02da27b00a9eff5b1631bf02f9cc952
dingjingMaster <dingjing@live.cn>
2018-01-23 17:50:03 +0800

    小修改

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -17,7 +17,7 @@ function search_init(){
                 '<div class="layui-form-item">'.
                     '<label class="layui-form-label">查询维度</label>'.
                     '<div class="layui-input-block">'.
-                        '<select name="dim" lay-verify="required">'.
+                        '<select id="form_dim" name="dim" lay-verify="required" onChange="exhibit"_change()">'.
                             '<option value=""></option>'.
                             '<option value="sum">总计</option>'.
                             '<option value="fee">付费类型</option>'.
@@ -30,6 +30,16 @@ function search_init(){
                         '</select>'.
                     '</div>'.
                 '</div>'.
+    
+                '<div class="layui-form-item">'.
+                    '<label class="layui-form-label">页面模块</label>'.
+                    '<div class="layui-input-block">'.
+                        '<select id="form_module" name="dim" lay-verify="required">'.
+                            '<option value=""></option>'.
+                        '</select>'.
+                    '</div>'.
+                '</div>'.
+
                 '<div class="layui-form-item">'.
                     '<label class="layui-form-label">付费类型</label>'.
                     '<div class="layui-input-block">'.
@@ -38,29 +48,6 @@ function search_init(){
                         '<input type="checkbox" name="monFee" title="包月">'.
                         '<input type="checkbox" name="pubFee" title="公版">'.
                         '<input type="checkbox" name="tfFee" title="限免">'.
-                    '</div>'.
-                '</div>'.
-
-                '<div class="layui-form-item">'.
-                    '<label class="layui-form-label">页面模块</label>'.
-                    '<div class="layui-input-block">'.
-                        '<input type="checkbox" name="shfRecMdl" title="书架推荐">'.
-                        '<input type="checkbox" name="freGusMdl" title="免费-猜你喜欢">'.
-                        '<input type="checkbox" name="bakArdMdl" title="章末页-读本书的人还看过">'.
-                        '<input type="checkbox" name="foeArdMdl" title="封面页-读本书的人还看过">'.
-                        '<input type="checkbox" name="shfGusMdl" title="书架-猜你喜欢">'.
-                        '<input type="checkbox" name="chsStmMdl" title="精选-瀑布流">'.
-                        '<input type="checkbox" name="foeArdMorMdl" title="封面页-读本书的人还看过更多">'.
-                        '<input type="checkbox" name="monStmMdl" title="包月瀑布流">'.
-                        '<input type="checkbox" name="foeCtgMdl" title="封面页-类别推荐">'.
-                        '<input type="checkbox" name="foeAutMdl" title="封面页-作者推荐">'.
-                        '<input type="checkbox" name="chsFinStmMdl" title="精选-完结瀑布流">'.
-                        '<input type="checkbox" name="freMonRecMdl" title="免费-包月推荐">'.
-                        '<input type="checkbox" name="chsBoyStmMdl" title="精选-男频瀑布流">'.
-                        '<input type="checkbox" name="chsRakStmMdl" title="精选-排行瀑布流">'.
-                        '<input type="checkbox" name="chsGilStmMdl" title="精选-女频瀑布流">'.
-                        '<input type="checkbox" name="redRecMdl" title="根据阅读推荐">'.
-                        '<input type="checkbox" name="extRecMdl" title="退出拦截推荐">'.
                     '</div>'.
                 '</div>'.
 
