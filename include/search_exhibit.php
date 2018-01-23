380247e232f04316d8a7f9a4833090ec7419ed12
dingjingMaster <dingjing@live.cn>
2018-01-23 14:14:16 +0800

    小修改

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -17,14 +17,14 @@ function search_init(){
             '<div class="layui-form-item">' . 
                 '<label class="layui-form-label">选择查询维度</label>' .
                 '<div class="layui-input-block">' . 
-                    '<input type="radio" name="dim" value="sumDim" title="模块总计"/>' . 
-                    '<input type="radio" name="dim" value="feeDim" title="付费类型"/>' .
-                    '<input type="radio" name="dim" value="stgDim" title="推荐策略"/>' .
-                    '<input type="radio" name="dim" value="stuDim" title="连载状态"/>' .
-                    '<input type="radio" name="dim" value="vewDim" title="订阅级别"/>' .
-                    '<input type="radio" name="dim" value="itmDim" title="入库时间"/>' .
-                    '<input type="radio" name="dim" value="utmDim" title="更新时间"/>' .
-                    '<input type="radio" name="dim" value="ctg1Dim" title="一级分类"/>'.
+                    '<input type="radio" name="dim" value="sumDim" title="模块总计">' . 
+                    '<input type="radio" name="dim" value="feeDim" title="付费类型">' .
+                    '<input type="radio" name="dim" value="stgDim" title="推荐策略">' .
+                    '<input type="radio" name="dim" value="stuDim" title="连载状态">' .
+                    '<input type="radio" name="dim" value="vewDim" title="订阅级别">' .
+                    '<input type="radio" name="dim" value="itmDim" title="入库时间">' .
+                    '<input type="radio" name="dim" value="utmDim" title="更新时间">' .
+                    '<input type="radio" name="dim" value="ctg1Dim" title="一级分类">'.
                 '</div>' .
             '</div>' .
          '</form>' .
