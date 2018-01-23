fc8a28f8e0c36ff330d63ec28b94fa126ad2d055
dingjingMaster <dingjing@live.cn>
2018-01-23 17:53:09 +0800

    小修改

--- js/main_search.js
+++ js/main_search.js
@@ -51,22 +51,22 @@ function exhibit_change() {
     var y = document.getElementById("form_module");
 
     y.options.length = 0;
-    if(x.selectedIndex == 0) {
-        y.options.add(new Option("书架推荐", "shfRecMdl"));
-        y.options.add(new Option("免费-猜你喜欢", "freGusMdl"));
-        y.options.add(new Option("章末页-读本书的人还看过", "bakArdMdl"));
-        y.options.add(new Option("封面页-读本书的人还看过", "foeArdMdl"));
+    if(x.selectedIndex == 1) {
         y.options.add(new Option("书架-猜你喜欢", "shfGusMdl"));
-        y.options.add(new Option("精选-瀑布流", "chsStmMdl"));
-        y.options.add(new Option("封面页-读本书的人还看过更多", "foeArdMorMdl"));
-        y.options.add(new Option("包月瀑布流", "monStmMdl"));
-        y.options.add(new Option("封面页-类别推荐", "foeCtgMdl"));
-        y.options.add(new Option("封面页-作者推荐", "foeAutMdl"));
-        y.options.add(new Option("精选-完结瀑布流", "chsFinStmMdl"));
         y.options.add(new Option("免费-包月推荐", "freMonRecMdl"));
+        y.options.add(new Option("免费-猜你喜欢", "freGusMdl"));
+        y.options.add(new Option("精选-瀑布流", "chsStmMdl"));
+        y.options.add(new Option("精选-完结瀑布流", "chsFinStmMdl"));
         y.options.add(new Option("精选-男频瀑布流", "chsBoyStmMdl"));
         y.options.add(new Option("精选-排行瀑布流", "chsRakStmMdl"));
         y.options.add(new Option("精选-女频瀑布流", "chsGilStmMdl"));
+        y.options.add(new Option("封面页-类别推荐", "foeCtgMdl"));
+        y.options.add(new Option("封面页-作者推荐", "foeAutMdl"));
+        y.options.add(new Option("封面页-读本书的人还看过", "foeArdMdl"));
+        y.options.add(new Option("封面页-读本书的人还看过更多", "foeArdMorMdl"));
+        y.options.add(new Option("章末页-读本书的人还看过", "bakArdMdl"));
+        y.options.add(new Option("书架推荐", "shfRecMdl"));
+        y.options.add(new Option("包月瀑布流", "monStmMdl"));
         y.options.add(new Option("根据阅读推荐", "redRecMdl"));
         y.options.add(new Option("退出拦截推荐", "extRecMdl"));
     }
