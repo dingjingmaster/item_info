b50e637d68ceadf0671eb25f36953a1037d27340
dingjingMaster <dingjing@live.cn>
2018-01-24 11:55:47 +0800

    联动

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -43,25 +43,23 @@ function search_init(){
                  '<div id="form_mod_div" class="layui-form-item">'.
                     '<label class="layui-form-label">查询模块</label>'.
                     '<div class="layui-input-block">'.
-                        '<select id="form_mod" lay-filter="form_mod" name="mod">'.
-                            '<option value="shfRecMdl">书架推荐</select>'.
-                            '<option value="monStmMdl">包月瀑布流</select>'.
-                            '<option value="redRecMdl">根据阅读推荐</select>'.
-                            '<option value="extRecMdl">退出拦截推荐</select>'.
-                            '<option value="freGusMdl">免费-猜你喜欢</select>'.
-                            '<option value="freMonRecMdl">免费-包月推荐</select>'.
-                            '<option value="shfGusMdl">书架-猜你喜欢</select>'.
-                            '<option value="chsStmMdl">精选-瀑布流</select>'.
-                            '<option value="chsBoyStmMdl">精选-男频瀑布流</select>'.
-                            '<option value="chsGilStmMdl">精选-女频瀑布流</select>'.
-                            '<option value="chsRakStmMdl">精选-排行瀑布流</select>'.
-                            '<option value="chsFinStmMdl">精选-完结瀑布流</select>'.
-                            '<option value="foeCtgMdl">封面页-类别推荐</select>'.
-                            '<option value="foeAutMdl">封面页-作者推荐</select>'.
-                            '<option value="foeArdMdl">封面页-读本书的人还看过</select>'.
-                            '<option value="foeArdMorMdl">封面页-读本书的人还看过更多</select>'.
-                            '<option value="bakArdMdl">章末页-读本书的人还看过</select>'.
-                        '</select>'.
+                        '<input type="checkbox" name="shfRecMdl" title="书架推荐"/>'.
+                        '<input type="checkbox" name="monStmMdl" title="包月瀑布流"/>'.
+                        '<input type="checkbox" name="redRecMdl" title="根据阅读推荐"/>'.
+                        '<input type="checkbox" name="extRecMdl" title="退出拦截推荐"/>'.
+                        '<input type="checkbox" name="freGusMdl" title="免费-猜你喜欢"/>'.
+                        '<input type="checkbox" name="freMonRecMdl" title="免费-包月推荐"/>'.
+                        '<input type="checkbox" name="shfGusMdl" title="书架-猜你喜欢"/>'.
+                        '<input type="checkbox" name="chsStmMdl" title="精选-瀑布流"/>'.
+                        '<input type="checkbox" name="chsBoyStmMdl" title="精选-男频瀑布流"/>'.
+                        '<input type="checkbox" name="chsGilStmMdl" title="精选-女频瀑布流"/>'.
+                        '<input type="checkbox" name="chsRakStmMdl" title="精选-排行瀑布流"/>'.
+                        '<input type="checkbox" name="chsFinStmMdl" title="精选-完结瀑布流"/>'.
+                        '<input type="checkbox" name="foeCtgMdl" title="封面页-类别推荐"/>'.
+                        '<input type="checkbox" name="foeAutMdl" title="封面页-作者推荐"/>'.
+                        '<input type="checkbox" name="foeArdMdl" title="封面页-读本书的人还看过"/>'.
+                        '<input type="checkbox" name="foeArdMorMdl" title="封面页-读本书的人还看过更多"/>'.
+                        '<input type="checkbox" name="bakArdMdl" title="章末页-读本书的人还看过"/>'.
                     '</div>'.
                 '</div>'.
                 ''.
