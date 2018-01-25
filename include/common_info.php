d82ec9b73aa3d8ba40683ae95dca140814713399
dingjingMaster <dingjing@live.cn>
2018-01-25 18:13:32 +0800

    基本流程完成

--- include/common_info.php
+++ include/common_info.php
@@ -23,6 +23,7 @@ function _common_print_head(){
         '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">' .
         '<link rel="stylesheet" href="lib/layui/css/layui.css" media="all">' .
         '<script src="lib/layui/layui.js"></script>'.
+        '<script src="lib/layui/lay/modules/laydate.js"></script>'.
         '<script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>'.
         '<script src="lib/highcharts/code/highcharts.js"></script>';
 
