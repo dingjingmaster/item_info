8dbc81ed64dc3d10dbf23038714faff43ef7e067
dingjingMaster <dingjing@live.cn>
2018-01-25 11:13:51 +0800

    后端数据准备

--- include/search_exhibit.php
+++ include/search_exhibit.php
@@ -151,11 +151,11 @@ function search_select($data){
         foreach($fee as $j) { // feeCate
             foreach($target as $k) { //直接字段
                 $msql = $sql . ' typeCate=' . exhibit_flag_to_number($i) . ' AND ' . ' feeCate=' . exhibit_flag_to_number($j);
-                return $msql;
                 $result = _mysql_query($msql);
                 $xArray = array();
                 $yArray = array();
                 $cate = '';
+                echo $msql;
                 while($row = _mysql_fetch_array($result)) {
                     return $row['dzid'];
                     $cate = exhibit_prekey_split($row['dzid']); //解析
