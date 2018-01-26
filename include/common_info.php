ecdfd1efdcf10fb46870ccb0703cef3d6d4ccd3a
dingjingMaster <dingjing@live.cn>
2018-01-26 10:04:29 +0800

    测试

--- include/common_info.php
+++ include/common_info.php
@@ -23,7 +23,7 @@ function _common_print_head(){
         '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">' .
         '<script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>'.
         '<link rel="stylesheet" href="lib/layui/css/layui.css" media="all"/>' .
-        '<script src="lib/layui/layui.all.js"></script>'.
+        '<script src="lib/layui/layui.js"></script>'.
         '<script src="lib/highcharts/code/highcharts.js"></script>';
 
     return '';
